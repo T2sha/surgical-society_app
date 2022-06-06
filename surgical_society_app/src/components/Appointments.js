9e3643e09805bc52a0b15fe3579ef110ba3c94e1
T2sha <21100474@virtualwindow.co.za>
2022-06-06 02:05:39 +0200

    Heading on Appointment page added

--- surgical_society_app/src/components/Appointments.js
+++ surgical_society_app/src/components/Appointments.js
@@ -1,6 +1,24 @@
+import React from 'react';
+import Navigation from './Navigation';
+import drip from '../Images/app.svg';
+
 const Appointments = ()=>{
     return(
         <>
+         <Navigation/>
+
+         <div className = "app-container">
+             <img className ="drip" src = {drip }></img>
+             <h2>Appointments</h2>
+
+
+         </div>
+
+    
+
+
+
+
 
         
         </>
