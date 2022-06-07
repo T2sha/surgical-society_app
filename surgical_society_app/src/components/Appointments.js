73e923a57c7c0b51162e187a63442541eb9ac8a2
T2sha <21100474@virtualwindow.co.za>
2022-06-07 22:58:23 +0200

    Updated Appointments Front end

--- surgical_society_app/src/components/Appointments.js
+++ surgical_society_app/src/components/Appointments.js
@@ -1,29 +1,55 @@
 import React from 'react';
 import Navigation from './Navigation';
 import drip from '../Images/app.svg';
-import doctor from '../Images/doctor.svg'
+import doctor from '../Images/doctor.svg';
+import { UilSearch } from '@iconscout/react-unicons'
 import Calendar from './Calendar';
 
 const Appointments = ()=>{
     return(
         <>
-         <Navigation/>
+         <Navigation/> 
 
          <div className = "app-container">
              <img className ="drip" src = {drip}></img>
              <h2>Appointments</h2>
-
-             <div className = "welcome">
+               <div className = "welcome">
                  <h3>Hi, Jennifer</h3>
                  <p>"May your day be blessed !"</p>
                  <img className="doctor" src={doctor}></img>
-             </div>
-             <h4>Calendar</h4>
 
-             <div className="calendar">
-            
              </div>
+
+
+             <h4>Calendar</h4>
+        <div className="calendar">
+            <Calendar/>
+
+             </div>
+
+        <div><input className="search-input" type="text" placeholder="Search..."/>
+            <div><button className="search-button"></button>
+                 <div className="search-icon"><UilSearch/></div>  
+                 </div>
+
+             </div>
+
+
              <h4 className="list">List of appointments</h4>
+            <div className="app-list">
+
+             </div>
+
+             <h4 className="new">Create new appointments</h4>
+             <div className="new-appointments">
+
+             </div>
+
+
+             
+            
+            
+             
 
 
 
--- surgical_society_app/src/components/Navigation.js
+++ surgical_society_app/src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  UilHospital,  UilUser, UilUserMd } from '@iconscout/react-unicons'
+import {  UilHospital,  UilUser, UilUserMd} from '@iconscout/react-unicons'
 import logo from '../Icons/logo.svg'
 
 const Navigation = () => {
@@ -13,10 +13,13 @@ const Navigation = () => {
             <ul>
                 <li><a href="/" className= "Icon1"> <UilHospital/> </a></li>
                 <li><a href="/Patients" className= "Icon2"><UilUser/></a></li>
-                <li><a href="/Doctors" className= "Icon3"><UilUserMd/></a></li>
+                <li><a href="/Doctors" className= "Icon3"><UilUserMd/></a></li> 
             </ul>
             </div>
         </div>
+
+        
+
         </>
     )
 }
