d8405dc3f0d719714cf3949d964b0a7abb37647e
T2sha <21100474@virtualwindow.co.za>
2022-07-04 22:49:25 +0200

    Update addUser.php

--- surgical_society_app/surgicalApi/addUser.php
+++ surgical_society_app/surgicalApi/addUser.php
@@ -1,18 +1,34 @@
 <?php
 
-include 'db_connection.php';
-
-header("Access-Control-Allow-Origin: * ");
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: * ");
+include 'db_connection.php';
 
 $request_body = file_get_contents('php://input');
 $data = json_decode($request_body);
 
-$names = $data->names;
-$email = $data->email;
-$contact = $data->contact;
-$username = $data->username;
+$first = $data->first;
+$last = $data->last;
+$age = $data->age;
+$gender = $data->gender;
+$phone = $data->phone;
 $password = $data->password;
+$email = $data->email;
+$image = $data->password;
 
+$age = intval($age);
+$phone = intval($age);
+$password = intval($password);
+
+
+$sql = "INSERT INTO receptionist(id, name, age, gender, email, password, `phone number`, rank) VALUES (NULL, '$first $last','$age','$gender','$email','$password','$phone','Assistan Receptionist');";
+$result = mysqli_query($conn, $sql);
+
+
+if(!$result) {
+    echo "Error";
+} else {
+    echo "It worked";
+}
 
 ?>
