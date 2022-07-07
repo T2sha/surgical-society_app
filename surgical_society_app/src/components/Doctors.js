ad3f503f34ba901c0744f1751dfdf423298b483e
T2sha <21100474@virtualwindow.co.za>
2022-07-07 11:46:47 +0200

    Updated Doctors Page

--- surgical_society_app/src/components/Doctors.js
+++ surgical_society_app/src/components/Doctors.js
@@ -6,6 +6,19 @@ const Doctors = ()=>{
          <Navigation/> 
 
         <div className="app-container">
+
+        <div className=" List-Patients">
+              <div className="Detailed-view">
+
+              </div>
+         </div>
+
+          <div className="Add-New-Patients">
+              <div className="form-patients">
+
+              </div>
+
+          </div>
             
         </div>
 
